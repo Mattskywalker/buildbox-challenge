66ccaabfd5f0f207a62347ac5824735e4d20a6bb
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-07 14:45:03 -0300

    post validation formik

--- src/components/WritePostCard/index.tsx
+++ src/components/WritePostCard/index.tsx
@@ -1,6 +1,6 @@
 import { IconButton, Stack, styled, TextField } from '@mui/material'
-import React, { ChangeEvent, useState } from 'react'
-import Trash from '../../assets/icons/trash'
+import React, { ChangeEvent, useContext, useState } from 'react'
+import Trash from '../../assets/icons/Trash'
 import Image from '../../assets/Image'
 import Button from '../Button'
 import Card from '../Card'
@@ -8,25 +8,57 @@ import Link from '../Link'
 import TextArea from '../TextArea'
 import TextInput from '../TextInput'
 
+import * as yup from 'yup'
+import { FormikProvider, useFormik } from 'formik'
+import { PostModel } from '../../Interfaces/PostInterfaces'
+import HelperText from '../HelperText'
+import { PostContext } from '../../Context/PostContext'
+
+
+const schema = yup.object().shape({
+  author: yup.string().required('É necessário o nome do autor para fazer uma publicação.'),
+  message: yup.string().required('Escreva uma mensagem para fazer uma publicação.'),
+})
+
 export default function WritePostCard() {
 
-  const [image, setImage] = useState('');
+  const {savePost} = useContext(PostContext);
+
   const [imageBlob, setImageBlob] = useState<File | null>(null);
-  const imageIsEmpty = image === '';
+  const [imagePath, setImagePath] = useState<string>('')
 
   function handleImage(e: React.ChangeEvent<HTMLInputElement>) {
+    setImagePath(e.target.value);
     const files = e.target.files;
     if (!files || files.length === 0) { return }
-
     setImageBlob(files[0]);
-    setImage(URL.createObjectURL(files[0]));
+    handleChange('imageUrl')(URL.createObjectURL(files[0]))
   }
 
   function handleRemoveImage() {
-    setImage('');
     setImageBlob(null);
+    setImagePath('')
+    handleChange('imageUrl')('');
   }
 
+  const formik = useFormik<PostModel>({
+    initialValues: {
+      author: '',
+      imageUrl: '',
+      message: ''
+    },
+    enableReinitialize: true,
+    validationSchema: schema,
+    validateOnChange: true,
+    initialTouched: {},
+    onSubmit: async (values, { setSubmitting }) => {
+      savePost(values)
+    }
+  });
+
+  
+  const { values, handleChange, errors, touched, setFieldTouched, handleSubmit } = formik
+  const imageIsEmpty = values.imageUrl === '';
   return (
     <Card gap={1} >
       <Stack alignItems={'center'} flexDirection={'row'} width={'100%'} >
@@ -42,25 +74,40 @@ export default function WritePostCard() {
             justifyContent={'center'}
           >
             {imageIsEmpty && <Image />}
-            {!imageIsEmpty && <img width={'100%'} src={image} alt={'message image'} />}
-            <input onChange={handleImage} hidden accept="image/*" type="file" />
+            {!imageIsEmpty && <img width={'100%'} src={values.imageUrl} alt={'message image'} />}
+            <input value={imagePath} onChange={(e) => handleImage(e)} hidden accept="image/*" type="file" />
           </Stack>
         </IconButton>
         <Stack flex={1} >
           {
             !imageIsEmpty &&
-              <IconButton onClick={() => handleRemoveImage()} style={{ height: '42px', width: '42px' }} >
-                <Trash />
-              </IconButton>
+            <IconButton onClick={() => handleRemoveImage()} style={{ height: '42px', width: '42px' }} >
+              <Trash />
+            </IconButton>
           }
         </Stack>
       </Stack>
-      <TextInput style={{ marginTop: '16px' }} placeholder='Digite seu nome' />
-      <TextArea placeholder='Mensagem' />
+      <FormikProvider value={formik} >
+        <TextInput
+          onBlur={() => setFieldTouched('author')}
+          value={values.author}
+          onChange={handleChange('author')}
+          style={{ marginTop: '16px' }}
+          placeholder='Digite seu nome'
+        />
+        <HelperText showError={!!errors.author && touched.author } message={errors.author} />
+
+        <TextArea
+          onBlur={() => setFieldTouched('message')}
+          value={values.message}
+          onChange={handleChange('message')}
+          placeholder='Mensagem' />
+        <HelperText showError={!!errors.message && touched.message } message={errors.message} />
+      </FormikProvider>
 
       <Stack pt={2} gap={2} width={'100%'} flexDirection={'row'} alignItems={'center'} justifyContent='flex-end' className={'area-button'} >
         <Link onClick={() => { alert('ok') }} >Descartar</Link>
-        <Button>Publicar</Button>
+        <Button onClick={() => handleSubmit()} >Publicar</Button>
       </Stack>
     </Card>
   )
