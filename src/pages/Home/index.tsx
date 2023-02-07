782eedd99bbedf4f7a47e8c4bfafb162d5ff2a2c
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-07 17:20:14 -0300

    user msg empty feed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import Typography from '@mui/material/Typography'
 import { Stack } from '@mui/system'
 import React, { useContext } from 'react'
+import LoadingScreen from '../../components/LoadingScreen'
 import Post from '../../components/Post'
 import Card from '../../components/WritePostCard'
 import { PostContext } from '../../Context/PostContext'
 import Page from '../../layouts/Page'
+
+import EmptyIcon from '../../assets/images/package.png'
 
 export default function Home() {
 
@@ -18,7 +21,7 @@ export default function Home() {
       <Stack pt={8} >
         <Typography color={'#9f9f9f'} >Feed</Typography>
 
-        <Stack style={{transition: '2s'}}  gap={2} >
+        <Stack style={{ transition: '2s' }} gap={2} >
           {
             getPosts().map((value, index) => (
               <Post
@@ -26,6 +29,13 @@ export default function Home() {
                 key={index}
               />
             ))
+          }
+          {
+            getPosts().length === 0 &&
+            <Stack pt={3} flexDirection={'row'} alignItems={'center'} gap={2}>
+              <img src={EmptyIcon} width={'42px'} />
+              <Typography color={'#9f9f9f'} >Nada para exibir agora. 😥</Typography>
+            </Stack>
           }
         </Stack>
       </Stack>
