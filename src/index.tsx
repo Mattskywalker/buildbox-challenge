1df291b0ac30563324a3e382f1eb68109ad0a5a7
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-07 14:44:16 -0300

    browse router

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
