6aa33c2ca68bdd641388232d589d408a35be5f24
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-08 01:33:26 -0300

    post loading animations

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,30 +1,48 @@
-import { IconButton, Typography } from '@mui/material'
+import { CircularProgress, IconButton, Skeleton, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
-import React, { useContext, useState } from 'react'
+import React, { memo, useContext, useEffect, useState } from 'react'
 import Close from '../../assets/icons/Close';
 import Image from '../../assets/Image';
 import { PostContext } from '../../Context/PostContext';
 import { PostModel } from '../../Interfaces/PostInterfaces';
 import Card from '../Card'
 
+import { Image as ImageMaterial } from '@mui/icons-material'
+import { toast } from 'react-toastify';
+
 interface PostProps {
   post: PostModel
 }
 
-export default function Post({ post }: PostProps) {
+function Post({ post }: PostProps) {
 
-  const {removePost} = useContext(PostContext);
-  const imageIsEmpty = post.imageUrl === null;
-  const [imageError, setImageError] = useState(imageIsEmpty);
+  const { removePost } = useContext(PostContext);
+  const [imageError, setImageError] = useState(false);
+  const [isImageLoading, setImageLoading] = useState(true);
+
+  const [deletingPost, setDeletingPost] = useState(false);
+
+  function handleRemovePost() {
+    setDeletingPost(true);
+    removePost(post)
+      .then(() => {
+        toast('Post Deletado.', { type: 'success', position: 'top-center', autoClose: 300 })
+      }).catch(() => {
+        toast('Falha ao remover Post.', { type: 'error', position: 'top-center', autoClose: 300 })
+      }).finally(() => { setDeletingPost(false) });
+  }
 
   return (
     <Card>
-      <Stack width={'100%'} height={{sm: '56px'}} alignItems={'flex-end'} >
-        <IconButton onClick={() => removePost(post)} >
+      <Stack justifyContent={'space-between'} flexDirection={'row'} width={'100%'} height={{ sm: '42px' }} alignItems={'flex-start'} >
+        {deletingPost && <CircularProgress color='error' size={'24px'} />}
+        {isImageLoading && !imageError && <CircularProgress color='success' size={'24px'} />}
+        <span />
+        <IconButton disabled={deletingPost} onClick={() => handleRemovePost()} >
           <Close />
         </IconButton>
       </Stack>
-      <Stack gap={4} width={'100%'} flexDirection={{sm: 'row', xs: 'column'}} >
+      <Stack gap={4} width={'100%'} flexDirection={{ sm: 'row', xs: 'column' }} >
         <Stack >
           <IconButton style={{ padding: 0 }} aria-label="upload picture" component="label" >
             <Stack
@@ -36,21 +54,33 @@ export default function Post({ post }: PostProps) {
               alignItems={'center'}
               justifyContent={'center'}
             >
+              {isImageLoading &&
+                <Stack alignItems={'center'} justifyContent={'center'} height={'100%'} width={'100%'}>
+                  <ImageMaterial style={{ position: 'absolute', zIndex: 1 }} />
+                  <Skeleton animation={'wave'} variant='rounded' height={'100%'} width={'100%'} />
+                </Stack>}
+
               {imageError && <Image />}
-              {!imageIsEmpty && !imageError && <img onError={() => setImageError(true)} width={'100%'} src={post.imageUrl} alt={'message image'} />}
+              <img
+                hidden={imageError || isImageLoading}
+                onLoad={() => setImageLoading(false)}
+                onError={() => {setImageLoading(false); setImageError(true)}}
+                width={'100%'}
+                src={post.imageUrl} alt={'message image'}
+              />
               {/* <input onChange={handleImage} hidden accept="image/*" type="file" /> */}
             </Stack>
           </IconButton>
         </Stack>
         <Stack width={'100%'} flex={1} gap={5} flexDirection={'column'} >
           <Stack width={'100%'} >
-            <Typography color={'#9f9f9f'}>
+            <Typography style={{wordWrap: 'break-word', wordBreak: 'break-all'}} color={'#9f9f9f'}>
               {post.message}
             </Typography>
           </Stack>
 
           <Stack>
-            <Typography color={'#9f9f9f'}>
+            <Typography variant={'caption'} color={'#9f9f9f'}>
               Enviado por:
             </Typography>
             <Typography color={'#9f9f9f'}>
@@ -62,3 +92,5 @@ export default function Post({ post }: PostProps) {
     </Card>
   )
 }
+
+export default memo(Post);
