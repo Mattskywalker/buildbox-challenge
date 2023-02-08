8381ac22d18143cb0f30d3ba162dd2ac78f7258d
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-08 01:30:10 -0300

    remove image

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,32 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { deleteObject, getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { toast } from "react-toastify";
 import { PostModel } from "../Interfaces/PostInterfaces";
 import { storage } from "./firebase";
 
 
-export async function savePostToFirebase(post: PostModel, image?: File) {
+export async function savePostImageToFirebase(post: PostModel, image?: File) {
 
-  const storageRef = ref(storage, post.id);
-
-  // 'file' comes from the Blob or File API
-  if(!!image) {
+  if (!!image) {
     await uploadBytes(ref(storage, `postImages/${post.id}`), image)
-      .then( async (data) => {
+      .then(async (data) => {
         post.imageUrl = await getDownloadURL(ref(storage, data.metadata.fullPath))
+      }).catch(() => {
+        toast('Falha ao fazer upload de imagem', { type: 'error', position: 'top-center' })
       })
   }
 
   return post;
 
+}
+
+
+export async function removePostImageFromFirebase(post: PostModel) {
+  let result = false;
+  await deleteObject(ref(storage, `postImages/${post.id}`))
+    .catch(() => {
+      toast('Não voi possível deletar imagem!', {type: 'error', position: 'top-center'});
+      result = true;
+    })
+
+  return result;
 }
